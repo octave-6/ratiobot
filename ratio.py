2add3b3c7a66fc81db0536ceb5aaafbb6c5911d7
Noah <96790645+octave-6@users.noreply.github.com>
2022-02-17 20:45:55 -0500

    ratio list updated
    ricardo is included to enhance the user experience

--- ratio.py
+++ ratio.py
@@ -1,13 +1,13 @@
-ratio = [
+ratio_list = [
     'ratio nerd', 'ratio idiot', 'ratio', 'didnt ask + ratio', 'L + ratio', 'catch this ratio', 'catch this ratio nerd', 
     'catch this ratio idiot', 'didn\'t ask + L + ratio', 'L + ratio, nerd', 'L + ratio', 'cumb + ratio', 'ray - she - oh',
     'L + ratio + ur bald + ur smelly + David fucks harder than you', 'L + ratio + u fell off + i cant be bothered to think of a new ratio bc its just too easy to ratio you',
     'L + ratio + you fell off + ratio + didnt ask + get cucked + eat dick shitlord + youll never know what parental approval feels like',
     'L + ratio + no one cares + didnt ask + Im a bot and I still get more bitches than you + The hood watches Dream SMP now',
-    'L + ratio + didnt ask + Hindemith is a cum stain + ur adopted + ur bald + you smell like stale cum and taco bell beans + get some bitches on your dick, nerd',
+    'L + ratio + didnt ask + Hindemith is a cum stain + ur bald + you smell like stale cum and taco bell beans',
     'L + ratio + you get negative pussy + david came on your pillowcase', '',
 ]
-wordle_ratio = [
+wordle_ratio_list = [
     'nobody cares about wordle + ratio', 'worlde is a dead meme + ratio idiot', 'ratio idiot, wordle is for plebs',
     'wordle more like ratio', 'you\'re about to be 6/6 on this ratio', 'L + ratio + wordle sucks',
     'didnt ask + ratio', 'didnt ask + ratio + wordle is for nerds', 'didnt ask + ratio + wordle sucks',
@@ -15,5 +15,25 @@ wordle_ratio = [
     '',
 ]
 
-print(len(ratio))
-print(len(wordle_ratio))
+ricardo = '''
+⠄⠄⠄⠄⠄⠄⢴⡶⣶⣶⣶⡒⣶⣶⣖⠢⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⢠⣿⣋⣿⣿⣉⣿⣿⣯⣧⡰⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⣿⣿⣹⣿⣿⣏⣿⣿⡗⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠟⡛⣉⣭⣭⣭⠌⠛⡻⢿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⣤⡌⣿⣷⣯⣭⣿⡆⣈⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿⣷⢛⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⢻⣷⣽⣿⣿⣿⢿⠃⣼⣧⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣛⣻⣿⠟⣀⡜⣻⢿⣿⣿⣶⣤⡀⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⢠⣤⣀⣨⣥⣾⢟⣧⣿⠸⣿⣿⣿⣿⣿⣤⡀⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⢟⣫⣯⡻⣋⣵⣟⡼⣛⠴⣫⣭⣽⣿⣷⣭⡻⣦⡀⠄
+⠄⠄⠄⠄⠄⠄⠄⢰⣿⣿⣿⢏⣽⣿⢋⣾⡟⢺⣿⣿⣿⣿⣿⣿⣷⢹⣷⠄
+⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢣⣿⣿⣿⢸⣿⡇⣾⣿⠏⠉⣿⣿⣿⡇⣿⣿⡆
+⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢸⣿⣿⣿⠸⣿⡇⣿⣿⡆⣼⣿⣿⣿⡇⣿⣿⡇
+⠇⢀⠄⠄⠄⠄⠄⠘⣿⣿⡘⣿⣿⣷⢀⣿⣷⣿⣿⡿⠿⢿⣿⣿⡇⣩⣿⡇
+⣿⣿⠃⠄⠄⠄⠄⠄⠄⢻⣷⠙⠛⠋⣿⣿⣿⣿⣿⣷⣶⣿⣿⣿⡇⣿⣿⡇
+'''
+
+if __name__ == '__main__':
+    '''prints max index for lists'''
+    print('ratio list:', len(ratio_list))
+    print('wordle_ratio:', len(wordle_ratio_list))
