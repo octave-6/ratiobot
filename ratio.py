0e4fc88569b4e7ca8afe546d48b3895aca03c8ce
Noah <96790645+octave-6@users.noreply.github.com>
2022-02-20 14:08:46 -0500

    Update ratio.py

--- ratio.py
+++ ratio.py
@@ -1,18 +1,8 @@
 ratio_list = [
-    'ratio nerd', 'ratio idiot', 'ratio', 'didnt ask + ratio', 'L + ratio', 'catch this ratio', 'catch this ratio nerd', 
-    'catch this ratio idiot', 'didn\'t ask + L + ratio', 'L + ratio, nerd', 'L + ratio', 'cumb + ratio', 'ray - she - oh',
-    'L + ratio + ur bald + ur smelly + David fucks harder than you', 'L + ratio + u fell off + i cant be bothered to think of a new ratio bc its just too easy to ratio you',
-    'L + ratio + you fell off + ratio + didnt ask + get cucked + eat dick shitlord + youll never know what parental approval feels like',
-    'L + ratio + no one cares + didnt ask + Im a bot and I still get more bitches than you + The hood watches Dream SMP now',
-    'L + ratio + didnt ask + Hindemith is a cum stain + ur bald + you smell like stale cum and taco bell beans',
-    'L + ratio + you get negative pussy + david came on your pillowcase', '',
+
 ]
 wordle_ratio_list = [
-    'nobody cares about wordle + ratio', 'worlde is a dead meme + ratio idiot', 'ratio idiot, wordle is for plebs',
-    'wordle more like ratio', 'you\'re about to be 6/6 on this ratio', 'L + ratio + wordle sucks',
-    'didnt ask + ratio', 'didnt ask + ratio + wordle is for nerds', 'didnt ask + ratio + wordle sucks',
-    'L + ratio + u fell off + the hood plays quordle now', 'L + ratio + u fell off + the hood doesnt play wordle anymore',
-    '',
+
 ]
 
 ricardo = '''
