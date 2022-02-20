c92418b7954de09e414b7677b8fd2c32683b6a5a
Noah <96790645+octave-6@users.noreply.github.com>
2022-02-20 14:08:22 -0500

    Update main.py
    try block added to catch future errors
    auto_repopulate_reply_metadata added, when userID replies to others an error is thrown unless this is set to True
    wordle ratio favorited the status being replied to, not the ratio. this is now fixed.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ access_token = os.environ.get('ratio_access')
 access_token_secret = os.environ.get('ratio_access_secret')
 # setting mantinus twitter ID
 # additional IDs can be added for more ratio goodness
-mantinus_id = ['ID',]
+mantinus_id = ['ID HERE',]
 
 # twitter auth
 auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
@@ -25,7 +25,7 @@ class Stalker(tweepy.Stream):
         Allows me to ensure bot connected A-Okay,
         Plus bonus meme Ricardo to warm my heart
         '''
-        print('...:::Stalker activated:::...')
+        print('\n...:::Stalker activated:::...')
         print(ricardo)
 
     def on_status(self, status):
@@ -33,46 +33,52 @@ class Stalker(tweepy.Stream):
         Awaits for tweets from mantinus himself, or other users indicated in list mantinus_id
         '''
 
-        # will @ the RTer with an empty tweet unless caught in this if statement:
+        # will @ the RTer with an empty tweet unless caught
         if status.text.startswith('RT'):
             return "Repeat after me: I will not ratio RTs"
         # status.user.id == user ID of the bot posting the ratios,
         # else the bot will endlessly ratio itself
-        if status.user.id == 'ID':
+        if status.user.id == 1494375567218679811:
             return 'Repeat after me: I will not ratio myself'
         
         # now we can begin the ratio
         else:
-            print(f'Tweet from {status.user.screen_name} received:')
-            time.sleep(0.6)
-            print(status.text, '  ::  ', status.id)
-            time.sleep(0.2)
-            print('. . . . . .')
+            try:
+                print(f'Tweet from {status.user.screen_name} received:')
+                time.sleep(0.6)
+                print(status.text, '  ::  ', status.id)
+                time.sleep(0.2)
+                print('. . . . . .')
+                time.sleep(0.5)
 
-            # if Wordle tweet, special ratios are utilized for a more personal experience
-            # else, a standard ratio is used
-            if status.text.startswith('Wordle'):
-                rand = random.randint(0,11)
-                api.update_status(f'@{status.user.screen_name} {wordle_ratio_list[rand]}', in_reply_to_status_id=status.id)
-                api.create_favorite(status.id)
-                time.sleep(1)
-                print('wordle ratio sent')
-            
-            # Add elif conditions here to further personalize your ratio experience
-            
-            else:
-                rand = random.randint(0,19)
-                ratio = api.update_status(f'@{status.user.screen_name} {ratio_list[rand]}', in_reply_to_status_id=status.id)
-                print(ratio.id)
-                api.create_favorite(ratio.id)
-                time.sleep(1)
-                print('ratio sent.')
+                # if Wordle tweet, special ratios are utilized for a more personal experience
+                # else, a standard ratio is used
+                if status.text.startswith('Wordle'):
+                    rand = random.randint(0,11)
+                    ratio = api.update_status(f'@{status.user.screen_name} {wordle_ratio_list[rand]}', in_reply_to_status_id=status.id, auto_populate_reply_metadata=True)
+                    api.create_favorite(ratio.id)
+                    time.sleep(1)
+                    print('wordle ratio sent')
+                
+                # Add elif conditions here to further personalize your ratio experience
+                
+                else:
+                    rand = random.randint(0,38)
+                    ratio = api.update_status(f'@{status.user.screen_name} {ratio_list[rand]}', in_reply_to_status_id=status.id, auto_populate_reply_metadata=True)
+                    api.create_favorite(ratio.id)
+                    time.sleep(1)
+                    print('ratio sent.')
+            except Exception as e:
+                print('yo you suck at writing code:', e)
 
-
-if __name__ == '__main__':
+def main():
     matninus_listener = Stalker(
         consumer_key, consumer_key_secret,
         access_token, access_token_secret
     )
 
     matninus_listener.filter(follow=mantinus_id, threaded=True)
+
+
+if __name__ == '__main__':
+    main()
