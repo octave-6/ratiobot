5b37270ccdfbb3f840e93c5dc42366a77a9d61b7
Noah <96790645+octave-6@users.noreply.github.com>
2022-03-02 21:59:31 -0800

    Update sentiment.py

--- sentiment_analyzer/sentiment.py
+++ sentiment_analyzer/sentiment.py
@@ -10,7 +10,7 @@ def sentiment_analysis():
         for tweet in f:
             grades.append(sia.polarity_scores(tweet)['compound'])
 
-
+    # catches division by zero before it happens
     if len(grades) == 0:
         print('LOL you fucked up your tweet writer')
         print('Anyway just take a 50% because I\'m lazy')
@@ -25,7 +25,9 @@ def sentiment_analysis():
             open('sentiment_analyzer/tweets.txt', 'w').close()
               
             return adjusted_average
-              
+
+        # if an error is thrown, likely means my math fucked up somewhere and I managed to round a number to zero
+        # assuming this to be the case, the only possible scenario this happens is a true 0% compound score
         except:
             print('adjusted average equates to zero. Everyone is having a very bad day.')
             open('sentiment/tweets.txt', 'w').close()
