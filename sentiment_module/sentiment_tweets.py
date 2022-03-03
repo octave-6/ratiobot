3f7564eb22d9e13876eaaecd86b8d6aa31336e0e
Noah <96790645+octave-6@users.noreply.github.com>
2022-03-02 22:57:00 -0800

    Update sentiment_tweets.py

--- sentiment_module/sentiment_tweets.py
+++ sentiment_module/sentiment_tweets.py
@@ -1,55 +1,44 @@
 zero = [
-    '', 'Cogswell is witnessing it\'s final days', 'Is everone dead?',
-    'Cogswell is on the verge', 'Cogswell will soon fall',
+    '',
 ]
 
 ten_and_under = [
-    '', 'Cogswell is on the verge of colapse', 'Ya\'ll good?',
-    'Cogswell is in it\'s final days', 'Jesus.',
+    '',
 ]
 
 twenty_and_under = [
-    '', 'Everyone is considering ending it all today apparently', '🎵 It\'s the end of the world as we know it 🎵',
-    'Everyone line up your therapy appointments.', 'maximum depression.',
+    '',
 ]
 
 thirty_and_under = [
-    '', '🎵 It\'s the end of the world as we know it 🎵', 'Everyone probably needs therapy.',
-    'You deserve ice cream, treat yourself tonight :)', 'extreme depression.',
+    '', 
 ]
 
 forty_and_under = [
-    '', 'That\'s very not poggers.', 'I highly recommend therapy.',
-    'Treat yourselves to ice cream, it might cheer you up :)', 'lots of depression', 
+    '', 
 ]
 
 fifty_and_under = [
-    '', 'That\'s very unpoggers.', 'Consider therapy before things get too bad',
-    'Things could always be worse?', 'No me gusta',
-]
+    '',
+]    
 
 sixty_and_under = [
-    '', 'That\'s unpoggers.', 'Therapy is never not a good idea, just sayin\''
-    'Things could be much worse I guess', 'At least we\'re on the better half of the coin :)',
+    '', 
 ]
 
 seventy_and_under = [
-    '', 'That\'s almost pog.', 'So close to greatness. . .',
-    'Things could definitely be worse', 'Life is looking better!',
+    '', 
 ]
 
 eighty_and_under = [
-    '', 'That\'s poggers.', 'C\'s get degrees :)))',
-    'Enjoy your day everyone :)', 'It\'s a good day for some ratio\'s.',
+    '',
 
 ]
 
 ninety_and_under = [
-    '', 'That\'s pretty poggers.', 'Glad everyone is in a good mood today :)',
-    'but why do you all seem so depressed still?', 'It\'s a PERFECT day for some ratio\'s.',
+   
 ]
 
 hundred_and_under = [
-    '', 'That\'s super poggers.', 'Dr. Cle*** must\'ve been fire today or something', 
-    'Did Matt finally stop talking?', 'Who\'s passing out the drugs?', 
+   
 ]
