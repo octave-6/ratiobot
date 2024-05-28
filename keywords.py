d882fe29e7f7d2ec4b3e9c7fb8b8c208c73d6c0b
Noah <96790645+octave-6@users.noreply.github.com>
2024-05-28 16:08:49 -0400

    Update keywords.py

--- keywords.py
+++ keywords.py
@@ -1,25 +1,23 @@
 # when detected, ratio bot will have customised responses
 # based on the topic of the detected keywords
-ukraine_keywords = ['russia', 'ukraine', 'putin',]
+ukraine_keywords = 
 cogswell_keywords = [
-    'lounge', 'lesson', 'lessons', 'wind ensemble', 'windensemble', 'wind-ensemble',
-    'symphony band', 'simp band',
+
 ]
 
 luke_keywords = [
-    'luke', 'deist', 'trumpet' 'eisensmith', 'dr. eisensmith', 'dr eisensmith', 'lucas',
-    'SSBM', 'SSBU', 'tpt', 'smash bros', 'smashbros',
+
 ]
 
 # fuckers think they're slick when they ain't
 ratio_bot_keywords = [
-    'ratio bot', 'ratiobot', 'r*tio b*t', 'ratio b*t', 'r*tio bot', 'tio b', 'ti* b', 't*o b', 't** b',
+
 ]
 
 ratio_keywords = [
-    'ratio ', ' ratio', 'ratio.'
+
 ]
 
 iup_keywords = [
-    'iup', 'driscoll', 'oak grove',
+
 ]
